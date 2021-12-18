ce03ebbef2738aaa24dca2e0bc8af793b4a410d0
Nikita <69010558+Kapibarra@users.noreply.github.com>
2021-12-18 18:18:23 +0300

    init hero slider

--- src/index.js
+++ src/index.js
@@ -1,6 +1,36 @@
 import "./scss/index.scss";
 import Inputmask from "inputmask";
+import Flickity from "flickity";
 
+
+/* hero slider */
+
+const options = {
+  accessibility: true,
+  prevNextButtons: true,
+  pageDots: true,
+  setGallerySize: false,
+  arrowShape: {
+    x0: 10,
+    x1: 60,
+    y1: 50,
+    x2: 60,
+    y2: 45,
+    x3: 15,
+  },
+};
+
+const carousel = document.querySelector('[data-carousel]');
+const slides = document.getElementsByClassName('carousel-cell');
+const flkty = new Flickity(carousel, options);
+
+flkty.on('scroll', function () {
+  flkty.slides.forEach(function (slide, i) {
+    const image = slides[i];
+    const x = (slide.target + flkty.x) * -1/3;
+    image.style.backgroundPosition = x + 'px';
+  });
+});
 /* Nav Scroll */
 
 (function($) {
